98d6f38082ee0985377d9cbc4b1843c7a51c391c
chaoma99 <chaoma99@gmail.com>
2016-03-31 14:17:38 +0800

    udpate

--- demo.m
+++ demo.m
@@ -1,5 +1,17 @@
 
 
+%   The code is provided for educational/researrch purpose only.
+%   If you find the software useful, please consider cite our paper.
+%
+%   Hierarchical Convolutional Features for Visual Tracking
+%   Chao Ma, Jia-Bin Huang, Xiaokang Yang, and Ming-Hsuan Yang
+%   IEEE International Conference on Computer Vision, ICCV 2015
+%
+% Contact:
+%   Chao Ma (chaoma99@gmail.com), or
+%   Jia-Bin Huang (jbhuang1@illinois.edu).
+
+
 addpath('utility','model','cf_scale','external/matconvnet/matlab');
 
 
--- run_tracker.m
+++ run_tracker.m
@@ -7,16 +7,6 @@
 % Output:
 %     - precision:          precision thresholded at 20 pixels
 %
-%   The code is provided for educational/researrch purpose only.
-%   If you find the software useful, please consider cite our paper.
-%
-%   Hierarchical Convolutional Features for Visual Tracking
-%   Chao Ma, Jia-Bin Huang, Xiaokang Yang, and Ming-Hsuan Yang
-%   IEEE International Conference on Computer Vision, ICCV 2015
-%
-% Contact:
-%   Chao Ma (chaoma99@gmail.com), or
-%   Jia-Bin Huang (jbhuang1@illinois.edu).
 %
 function [precision, fps] = run_tracker(video, show_visualization, show_plots)
 
